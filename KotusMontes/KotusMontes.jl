18dd345b31a237675b6ea9628d5fd04e6fa994a8
Renato Leriche <rlerichev@gmail.com>
2026-01-08 15:33:21 -0600

    KotusMontes project: Some definitions and descriptions.

--- KotusMontes/KotusMontes.jl
+++ KotusMontes/KotusMontes.jl
@@ -61,6 +61,14 @@ function createflm(l::Number, m::Number)
 	end
 end
 
+# ╔═╡ a2cf3c8b-c5c8-4df5-951e-62a7954e1083
+function createflm2(l::Number, m::Number)
+	function f(z::Number)
+		lez = l*exp(z)
+		l*exp(lez+m/z)+m*z/(lez*z+m)
+	end
+end
+
 # ╔═╡ 9bceda1a-3a79-4e35-95b2-2572d5577993
 function createf´lm(l::Number, m::Number)
 	function f(z::Number)
@@ -353,6 +361,37 @@ md"""
 ## Dynamic plane
 """
 
+# ╔═╡ 0dc154e3-b602-42be-9b56-3325f1961581
+#=let
+	l,m = -20,0.25
+	
+	xmin,xmax,ymin,ymax = -12,2,-6,6
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
+	Δ = min(Δx,Δy)
+	xs = xmin:Δ:xmax
+	ys = ymin:Δ:ymax
+	
+	fig = Figure(size=(800,800))
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax))
+
+	f2 = createflm2(l,m)
+	
+	trappedpoints!(ax, f2, xs, ys, hasescaped=z->real(z)<-100, maxiterations=32,
+		colormap=vermeerx #Gr.reverse(:vangogh)
+	)
+
+	c1 = findcritic1b(l, m, maxiterations=20)
+	c2 = findcriticN(2, l, m, maxiterations=100, maxiterationscompass=16, ε=0.00000001)
+
+	orbitpath!(ax, f2, c1, colormap=[:red], linewidth=0.5, markersize=4, iterations=20)
+	orbitpath!(ax, f2, c2, colormap=[:magenta], linewidth=0.5, markersize=4, iterations=20)
+
+	scatter!(ax, [Point2f(real(c1), imag(c1)), Point2f(real(c2), imag(c2))], color=:red, markersize=8)	
+	scatter!(ax, [Point2f(real(c1), -imag(c1)), Point2f(real(c2), -imag(c2))], color=:green, markersize=8)	
+	
+	fig
+end=#
+
 # ╔═╡ 369f53e9-4ea9-4462-8297-ebb3fc381ffb
 function stops(f::Function, z0::Number, N::Integer=1; ε::Real=0.000001)
 	z = z0
@@ -367,7 +406,7 @@ function stops(f::Function, z0::Number, N::Integer=1; ε::Real=0.000001)
 end
 
 # ╔═╡ edf8fc8f-b6bd-4f29-af08-00039b30acd1
-let
+#=let
 	l,m = -20,0.25 #-1.5,5 #-2,0.5 #0.1,4 #-4,4.5 #-4,1 #-3,0.1 #-0.25,1 # -1,0.25
 	
 	xmin,xmax,ymin,ymax = -12,2,-6,6
@@ -393,11 +432,61 @@ let
 	scatter!(ax, [Point2f(real(c1), -imag(c1)), Point2f(real(c2), -imag(c2))], color=:green, markersize=8)	
 	
 	fig
-end
+end=#
+
+# ╔═╡ 70a90845-4d5f-44e0-997b-4ff969b56346
+#=let
+	l,m = -20,0.25 #-1.5,5 #-2,0.5 #0.1,4 #-4,4.5 #-4,1 #-3,0.1 #-0.25,1 # -1,0.25
+	
+	xmin,xmax,ymin,ymax = -12,2,-6,6
+	Δx,Δy = (xmax-xmin)/720, (ymax-ymin)/720
+	Δ = min(Δx,Δy)
+	xs = xmin:Δ:xmax
+	ys = ymin:Δ:ymax
+	
+	fig = Figure(size=(1200,800))
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax))
+
+	f2 = createflm2(l,m)
+	
+	trappedpoints!(ax, f2, xs, ys, hasescaped=z->stops(f2,z,2), maxiterations=32, colormap=vermeerx)
+
+	c1 = findcritic1b(l, m, maxiterations=20)
+	c2 = findcriticN(2, l, m, maxiterations=100, maxiterationscompass=16, ε=0.00000001)
+
+	orbitpath!(ax, f2, c1, colormap=[:red], linewidth=0.5, markersize=4, iterations=20)
+	orbitpath!(ax, f2, c2, colormap=[:magenta], linewidth=0.5, markersize=4, iterations=20)
+
+	scatter!(ax, [Point2f(real(c1), imag(c1)), Point2f(real(c2), imag(c2))], color=:red, markersize=8)	
+	scatter!(ax, [Point2f(real(c1), -imag(c1)), Point2f(real(c2), -imag(c2))], color=:green, markersize=8)	
+	
+	fig
+end=#
 
 # ╔═╡ ffc2b1aa-c1d9-4c69-bccd-94d24b9744aa
 md"""
 ## Parameter plane
+Let $c_1=c_1(\lambda,\mu),\,c_2=c_2(\lambda,\mu)\in\mathbb{C}$ be the critical points of $f_{\lambda,\mu}$ such that $Im(c_1)\in(0,\pi)$ and $Im(c_2)\in(\pi,3\pi)$.
+"""
+
+# ╔═╡ cdf5ac1b-df7f-4270-8c67-1340e20861b4
+md"""
+In black, the "Mandelbrot set" of $f_{\lambda,\mu}$:
+
+$\mathcal{M}_1=\{(\lambda,\mu)\in\mathbb{R}^2|\,\,o(c_1,f_{\lambda,\mu})\,\,\mathrm{is\,bounded}\}.$
+
+In colors, using the escape time:
+
+$\mathbb{R}^2-\mathcal{M}_1.$
+"""
+
+# ╔═╡ 0fd2c626-52e9-4f8c-8eac-64a2047d9729
+md"""
+Reference: The Mandelbrot set
+
+$\mathcal{M}=\{c\in\mathbb{C}|\,\,o(0,q_c)\,\,\mathrm{is\,bounded}\}=\{z\in\mathbb{C}|\,\,q_c^n(0)\nrightarrow\infty\},$
+
+where $q_c(z)=z^2+c$.
 """
 
 # ╔═╡ d91916f8-37a6-4494-ac61-6df7aba6a0c1
@@ -422,7 +511,7 @@ md"""
 end=#
 
 # ╔═╡ 6e0bf940-1d94-4efa-a0a0-6558ba5d67fe
-let
+#=let
 	xmin,xmax,ymin,ymax = -6,0,0,6
 	Δx,Δy = (xmax-xmin)/720, (ymax-ymin)/720
 	Δ = min(Δx,Δy)
@@ -440,10 +529,52 @@ let
 	)
 
 	fig
-end
+end=#
+
+# ╔═╡ ffbacbc1-1b56-4b1f-93f1-0dce2f44bc9f
+md"""
+Let $z_0=z_0(\lambda,\mu)\in\mathbb{C}$ be the convergence point of the sequence $f_{\lambda,\mu}^{2n}(c_1)$, such that $|(f_{\lambda,\mu}^{2n})'(z_0)|<1$ or $z_0=0$.
+
+In colors (coloring with $n\mapsto|f_{\lambda,\mu}^n(c_1)-z_0|<\varepsilon$):
+
+$\mathcal{M}_2=\{(\lambda,\mu)\in\mathbb{R}^2|\,\, f_{\lambda,\mu}^{2n}(c_1)\rightarrow z_0\}.$
+
+In black:
+
+$\mathbb{R}^2-\mathcal{M}_2.$
+
+In red, the line of parameters where $f_{\lambda,\mu}$ has fixed parabolic points with multiplier $-1$:
+
+$\mathcal{L}_{-1}=\{(\lambda,\mu)\in\mathbb{R}^2|\,\,\exists z:\,\,f_{\lambda,\mu}(z)=z,\,\,f'_{\lambda,\mu}(z)=-1\}.$
+"""
+
+# ╔═╡ 249dda01-d980-4cc6-86f0-1df2a31dd7d6
+md"""
+Reference: in colors the Mandelbrot set's bulbs of period $1$ and $2$, union its exterior:
+
+$\{z\in\mathbb{C}|\,\,q_c^{2n}(0)\rightarrow z_c\,\,\mathrm{or}\,\,q_c^{2n}(0)\rightarrow\infty\},$
+
+where $q_c(z)=z^2+c$ and $z_c$ is an attracting fixed point of $q^2_c$.
+"""
+
+# ╔═╡ 855b2724-3129-4166-b170-99f6ae6cbc37
+md"""
+Let $z_0=z_0(\lambda,\mu)\in\mathbb{C}$ be the convergence point of the sequence $f_{\lambda,\mu}^{2n}(c_1)$, such that $|(f_{\lambda,\mu}^{2n})'(z_0)|<1$ and $z_0\neq0$.
+
+In colors (coloring with $n\mapsto|f_{\lambda,\mu}^n(c_1)-z_0|<\varepsilon$):
+
+$\mathcal{M}_{2,z_0\neq0}=\{(\lambda,\mu)\in\mathbb{R}^2|\,\, f_{\lambda,\mu}^{2n}(c_1)\rightarrow z_0\}\subset\mathcal{M}_2.$
+
+In black:
+
+$\mathbb{R}^2-\mathcal{M}_{2,z_0\neq0}.$
+
+In red, the line $\mathcal{L}_{-1}$.
+"""
 
 # ╔═╡ de1c003f-d951-47cf-acc3-beaea021298b
-let
+# "Vortex"
+#=let
 	xmin,xmax,ymin,ymax = -2,-1,4,5
 	Δx,Δy = (xmax-xmin)/720, (ymax-ymin)/720
 	Δ = min(Δx,Δy)
@@ -461,7 +592,18 @@ let
 	)
 
 	fig
-end
+end=#
+
+# ╔═╡ ee95a23b-177b-474d-a517-5cc9204831dd
+
+
+# ╔═╡ fab68d9a-311d-451f-9289-caaec83569dc
+marcopoints = [[-20,0.25], [-10,0.25], [-5ℯ/2,0.25], [-2ℯ,0.25], [-3ℯ/2,0.25], [-5ℯ/4,0.25], [-9ℯ/8,0.25], 
+[-ℯ,0.25], [-7ℯ/8,0.25], [-3ℯ/4,0.25], [-1,1-1/ℯ], [-1,0.75], [-1,7/8], [-1,15/16], [-1,0.5] ]
+
+# ╔═╡ d8613d71-af5e-4eb9-b065-b6467eae28b0
+renatopoints = [[-3ℯ/2,4.5], [-4.05,4.75], [-2ℯ,2], [-ℯ,0.0625], [-3ℯ/4,0.5], [-ℯ/2,1],
+	[-ℯ/4,0.25], [-ℯ/4,0.75], [-ℯ/4,1], [-ℯ/4,1.75], [-ℯ/8,2], [-ℯ/2,1.5], [-3ℯ/4,4], [-5ℯ/8,5]]
 
 # ╔═╡ 7a0d58fd-7238-4f0c-accf-9e429ca4d325
 md"""
@@ -471,55 +613,28 @@ md"""
 # ╔═╡ a07e4983-d801-47ba-9531-16bdfb1f5f26
 const Gr = SDDGraphics
 
-# ╔═╡ 99dc3a03-9aa0-4f52-8ed7-ffb4d4e5b538
+# ╔═╡ c14eacb3-f173-4609-b750-413027a296ca
+prismx = push!(RGBA.(deepcopy(colorschemes[:prism].colors)), RGBA(1,1,1,0.25))
+
+# ╔═╡ fd700d57-b159-4a0e-87e6-6b7361254b60
+vermeerx = pushfirst!(push!(RGBA.(Gr.reverse(:vermeer)), RGBA(0,0,0,0.9)),RGBA(1,1,1,0.1))
+
+# ╔═╡ 70f58eb7-adc8-451c-94a6-11344bbb200b
 let
-	xmin,xmax,ymin,ymax = -10,0,0,10
-	Δx,Δy = (xmax-xmin)/920, (ymax-ymin)/920
+	l,m = -20,0.25 # -4,4.5
+	
+	xmin,xmax,ymin,ymax = -8,4,-12,12
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
 	Δ = min(Δx,Δy)
 	xs = xmin:Δ:xmax
 	ys = ymin:Δ:ymax
 	
 	fig = Figure(size=(800,800))
-	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
-
-	mandelbrot!(ax,
-		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
-		seed = lm -> findcritic1b(real(lm), imag(lm), maxiterations=20, ε=0.001),
-		hasescaped=(c,z)->real(z)<-16, # ||real(z)>16, 
-		maxiterations=80, colormap=Gr.reverse(:vangogh)
-	)
-
-	save("test.jpg", fig)
-	
-	fig
-end
-
-# ╔═╡ 6b8acca5-4fd3-4812-a6c9-7f75f6b17b64
-mandelbrot((c,z)->z^2+c, -2.01:0.005:0.51,-1.16:0.005:1.16, seed=0, colormap=Gr.reverse(:vangogh),
-	hasescaped = (c,z) -> abs2(z)>4, maxiterations=60, axis=(;aspect=DataAspect()))
-
-# ╔═╡ c14eacb3-f173-4609-b750-413027a296ca
-prismx = push!(RGBA.(deepcopy(colorschemes[:prism].colors)), RGBA(1,1,1,0.25))
-
-# ╔═╡ fd700d57-b159-4a0e-87e6-6b7361254b60
-vermeerx = push!(RGBA.(Gr.reverse(:vermeer)), RGBA(0,0,0,0.5))
-
-# ╔═╡ 70f58eb7-adc8-451c-94a6-11344bbb200b
-let
-	l,m = -20,0.25
-	
-	xmin,xmax,ymin,ymax = -12,2,-6,6
-	Δx,Δy = (xmax-xmin)/720, (ymax-ymin)/720
-	Δ = min(Δx,Δy)
-	xs = xmin:Δ:xmax
-	ys = ymin:Δ:ymax
-	
-	fig = Figure(size=(1200,800))
 	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax))
 
 	f = createflm(l,m)
 	
-	trappedpoints!(ax, f, xs, ys, hasescaped=z->real(z)<-12, maxiterations=120,
+	trappedpoints!(ax, f, xs, ys, hasescaped=z->real(z)<max(-64,min(-8,l/m*4)), maxiterations=64,
 		colormap=vermeerx #Gr.reverse(:vangogh)
 	)
 
@@ -535,50 +650,105 @@ let
 	fig
 end
 
-# ╔═╡ 2b61308c-3efa-47a9-88d8-0ea870ca3d77
-mandelbrot((c,z)->z^2+c, -2.01:0.005:0.51,-1.16:0.005:1.16, seed=0, colormap=vermeerx, #Gr.reverse(:vermeer),
-	hasescaped = (c,z) -> stops(z->z^2+c, z, 2, ε=0.00001) || abs2(z)>4, maxiterations=240, axis=(;aspect=DataAspect()))
-
-# ╔═╡ 2382c3cc-266c-4f82-9c2f-daf60ded2ee9
+# ╔═╡ 99dc3a03-9aa0-4f52-8ed7-ffb4d4e5b538
 let
 	xmin,xmax,ymin,ymax = -6,0,0,6
-	Δx,Δy = (xmax-xmin)/720, (ymax-ymin)/720
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
 	Δ = min(Δx,Δy)
 	xs = xmin:Δ:xmax
 	ys = ymin:Δ:ymax
 	
-	fig = Figure(size=(1000,800))
+	fig = Figure(size=(800,800))
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
+
+	mandelbrot!(ax,
+		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
+		seed = lm -> findcritic1b(real(lm), imag(lm), maxiterations=20, ε=0.001),
+		hasescaped=(c,z)->real(z)<-16, # ||real(z)>16, 
+		maxiterations=200, colormap=vermeerx #Gr.reverse(:vangogh)
+	)
+
+	#g(x)=-2/x
+	#lines!(ax,xs,g.(xs),color=:yellow)
+
+	#save("test.jpg", fig)
+	
+	fig
+end
+
+# ╔═╡ 6b8acca5-4fd3-4812-a6c9-7f75f6b17b64
+mandelbrot((c,z)->z^2+c, -2.01:0.005:0.51,-1.16:0.005:1.16, seed=0, colormap=vermeerx,#Gr.reverse(:vangogh),
+	hasescaped = (c,z) -> abs2(z)>4, maxiterations=60, axis=(;aspect=DataAspect(), limits=(-2.01,0.51,-1.16,1.16)))
+
+# ╔═╡ 2382c3cc-266c-4f82-9c2f-daf60ded2ee9
+let
+	xmin,xmax,ymin,ymax = -6,0,0,6
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
+	Δ = min(Δx,Δy)
+	xs = xmin:Δ:xmax
+	ys = ymin:Δ:ymax
+	
+	fig = Figure(size=(800,800))
 	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
 
 	mandelbrot!(ax,
 		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
 		seed = lm -> findcriticBif(0.001,2.999, real(lm), imag(lm), maxiterations=32, ε=0.00001),
-		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.00001), 
-		maxiterations=40, colormap=vermeerx #:vangogh
+		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.0000025), 
+		maxiterations=200, colormap=vermeerx
 	)
+
+	lines!(ax,[-ℯ,0],[0,1],color=:red,linewidth=1.5)
 
 	fig
 end
 
-# ╔═╡ 74ebf7f1-da95-40fe-bc67-2e2dee196df6
+# ╔═╡ 2b61308c-3efa-47a9-88d8-0ea870ca3d77
+mandelbrot((c,z)->z^2+c, -2.01:0.005:0.51,-1.16:0.005:1.16, seed=0, colormap=vermeerx,
+	hasescaped = (c,z) -> stops(z->z^2+c, z, 2, ε=0.00001) || abs2(z)>4, maxiterations=240, axis=(;aspect=DataAspect(), limits=(-2.01,0.51,-1.16,1.16)))
+
+# ╔═╡ 35911590-c861-465d-921c-de93b66de7e7
 let
 	xmin,xmax,ymin,ymax = -6,0,0,6
-	Δx,Δy = (xmax-xmin)/720, (ymax-ymin)/720
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
 	Δ = min(Δx,Δy)
 	xs = xmin:Δ:xmax
 	ys = ymin:Δ:ymax
 	
-	fig = Figure(size=(1000,800))
+	fig = Figure(size=(800,800))
 	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
 
 	mandelbrot!(ax,
-		(lm,z) -> f2(real(lm), imag(lm), z), xs, ys,
+		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
 		seed = lm -> findcriticBif(0.001,2.999, real(lm), imag(lm), maxiterations=32, ε=0.00001),
-		hasescaped = (lm,z) -> abs(real(f(real(lm), imag(lm), z)))>16,
-		#hasescaped = (lm,z) -> stops(z->f2(real(lm), imag(lm), z), z, 2, ε=0.00001),
-		#|| real(f(real(lm), imag(lm), z))<-16, 
-		maxiterations=40, colormap=vermeerx #:vangogh
+		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.0000025) && real(f(real(lm), imag(lm), z))<-16, 
+		maxiterations=200, colormap=vermeerx
 	)
+
+	lines!(ax,[-ℯ,0],[0,1],color=:red,linewidth=1.5)
+
+	fig
+end
+
+# ╔═╡ 3d076c9b-263b-4d62-a17e-afbd7fee2f21
+let
+	xmin,xmax,ymin,ymax = -6,0,0,6
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
+	Δ = min(Δx,Δy)
+	xs = xmin:Δ:xmax
+	ys = ymin:Δ:ymax
+	
+	fig = Figure(size=(800,800))
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
+
+	mandelbrot!(ax,
+		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
+		seed = lm -> findcriticBif(0.001,2.999, real(lm), imag(lm), maxiterations=32, ε=0.00001),
+		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.0000025) && abs2(f(real(lm), imag(lm), z))<36, 
+		maxiterations=200, colormap=vermeerx
+	)
+
+	lines!(ax,[-ℯ,0],[0,1],color=:red,linewidth=1.5)
 
 	fig
 end
@@ -592,7 +762,7 @@ let
 	ys = ymin:Δ:ymax
 	
 	fig = Figure(size=(1000,800))
-	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:0.25:xmax, yticks=ymin:0.25:ymax)
 
 	mandelbrot!(ax,
 		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
@@ -606,23 +776,48 @@ end
 
 # ╔═╡ 2c9f1a59-ad76-4476-8d32-7e158dcd2541
 let
-	xmin,xmax,ymin,ymax = -5,0,0,2
+	xmin,xmax,ymin,ymax = -5,0,0,3
 	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
 	Δ = min(Δx,Δy)
 	xs = xmin:Δ:xmax
 	ys = ymin:Δ:ymax
 	
-	fig = Figure(size=(1000,600))
-	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
+	fig = Figure(size=(800,600))
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:0.5:xmax, yticks=ymin:0.5:ymax)
 
 	mandelbrot!(ax,
 		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
 		seed = lm -> findcriticBif(0.001,2.999, real(lm), imag(lm), maxiterations=32, ε=0.000001),
-		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.00001), # || real(z)<-16, 
-		maxiterations=120, colormap=vermeerx #Gr.reverse(:vermeer) #prismx #:vangogh
+		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.0000025), # || real(z)<-16, 
+		maxiterations=200, colormap=vermeerx #Gr.reverse(:vermeer) #prismx #:vangogh
 	)
 
-	#lines!(ax, [-ℯ,0], [0,2/ℯ], color=:red)
+	lines!(ax, [-ℯ,0], [0,1], color=:red, linewidth=1.5)
+
+	fig
+end
+
+# ╔═╡ ee2b8e91-d405-4045-9287-e72daf20c7fd
+let
+	xmin,xmax,ymin,ymax = -6,0,0,6
+	Δx,Δy = (xmax-xmin)/800, (ymax-ymin)/800
+	Δ = min(Δx,Δy)
+	xs = xmin:Δ:xmax
+	ys = ymin:Δ:ymax
+	
+	fig = Figure(size=(800,800))
+	ax = Makie.Axis(fig[1,1], aspect=DataAspect(), limits=(xmin,xmax,ymin,ymax), xticks=xmin:1:xmax, yticks=ymin:1:ymax)
+
+	mandelbrot!(ax,
+		(lm,z) -> f(real(lm), imag(lm), z), xs, ys,
+		seed = lm -> findcriticBif(0.001,2.999, real(lm), imag(lm), maxiterations=32, ε=0.00001),
+		hasescaped = (lm,z) -> stops(z->f(real(lm), imag(lm), z), z, 2, ε=0.0000025), 
+		maxiterations=200, colormap=vermeerx
+	)
+
+	lines!(ax,[-ℯ,0],[0,1],color=:red,linewidth=1.5)
+	scatter!(ax, Point2f.(marcopoints), color=1:length(marcopoints), colormap=:prism)
+	scatter!(ax, Point2f.(renatopoints), color=:green, marker=:cross, markersize=12)
 
 	fig
 end
@@ -634,6 +829,7 @@ end
 # ╠═729aaaf9-28f3-4274-9a26-12fc6f80f074
 # ╠═4662f2ab-8b41-4211-98e0-58778d61d007
 # ╠═f09e37b5-b0af-46c3-bf25-51017bf48ffa
+# ╠═a2cf3c8b-c5c8-4df5-951e-62a7954e1083
 # ╠═9bceda1a-3a79-4e35-95b2-2572d5577993
 # ╠═cada7f65-ccb1-4ec3-98e6-2370aac392cc
 # ╟─8a8619e8-0236-4c37-bd09-65d82110fc2a
@@ -657,19 +853,31 @@ end
 # ╠═4cc91117-7c7b-4e70-820c-aff85860ab02
 # ╟─208f363e-dc10-4307-aa27-3299ba2057a5
 # ╠═70f58eb7-adc8-451c-94a6-11344bbb200b
+# ╠═0dc154e3-b602-42be-9b56-3325f1961581
 # ╠═369f53e9-4ea9-4462-8297-ebb3fc381ffb
 # ╠═edf8fc8f-b6bd-4f29-af08-00039b30acd1
+# ╠═70a90845-4d5f-44e0-997b-4ff969b56346
 # ╟─ffc2b1aa-c1d9-4c69-bccd-94d24b9744aa
+# ╟─cdf5ac1b-df7f-4270-8c67-1340e20861b4
 # ╠═99dc3a03-9aa0-4f52-8ed7-ffb4d4e5b538
+# ╟─0fd2c626-52e9-4f8c-8eac-64a2047d9729
 # ╠═6b8acca5-4fd3-4812-a6c9-7f75f6b17b64
 # ╠═d91916f8-37a6-4494-ac61-6df7aba6a0c1
-# ╠═2b61308c-3efa-47a9-88d8-0ea870ca3d77
 # ╠═6e0bf940-1d94-4efa-a0a0-6558ba5d67fe
+# ╟─ffbacbc1-1b56-4b1f-93f1-0dce2f44bc9f
 # ╠═2382c3cc-266c-4f82-9c2f-daf60ded2ee9
-# ╠═74ebf7f1-da95-40fe-bc67-2e2dee196df6
+# ╟─249dda01-d980-4cc6-86f0-1df2a31dd7d6
+# ╠═2b61308c-3efa-47a9-88d8-0ea870ca3d77
+# ╠═35911590-c861-465d-921c-de93b66de7e7
+# ╟─855b2724-3129-4166-b170-99f6ae6cbc37
+# ╠═3d076c9b-263b-4d62-a17e-afbd7fee2f21
 # ╠═05efe4d3-4126-449a-ac48-1799c4c2f28a
-# ╠═de1c003f-d951-47cf-acc3-beaea021298b
+# ╟─de1c003f-d951-47cf-acc3-beaea021298b
 # ╠═2c9f1a59-ad76-4476-8d32-7e158dcd2541
+# ╠═ee95a23b-177b-474d-a517-5cc9204831dd
+# ╠═fab68d9a-311d-451f-9289-caaec83569dc
+# ╠═d8613d71-af5e-4eb9-b065-b6467eae28b0
+# ╠═ee2b8e91-d405-4045-9287-e72daf20c7fd
 # ╠═d52d0e77-9a16-4163-b2ef-10779cd1cdff
 # ╠═7a0d58fd-7238-4f0c-accf-9e429ca4d325
 # ╠═7af6c311-4ff3-4f71-84f0-8d9ef2d591c8
